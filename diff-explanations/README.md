008d33b10b3a160a56c0a7370f3c9aadc5509911
anderssonvivi <viviandersson0@gmail.com>
2024-03-20 17:56:56 +0100

    script for running docker builds and diffing

---
+++ diff-explanations/README.md
@@ -0,0 +1,3 @@
+TODO
+
+At the moment a very large diff...
---
+++ scripts/docker-and-diff.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <target name> <docker relative dir>"
+    exit 1
+fi
+
+open -a Docker
+sleep 2
+
+# vaiables
+TARGET=$1
+DIR=$2
+DOCKER_PATH=~/reproducible-go-ethereum/docker/$DIR
+OUTPUT_DIR=~/reproducible-go-ethereum/bin
+DIFF_DIR=~/reproducible-go-ethereum/diff-explanations
+rm ~/reproducible-go-ethereum/bin/geth-local && rm ~/reproducible-go-ethereum/bin/geth-reference
+mkdir -p $OUTPUT_DIR $DIFF_DIR 
+
+
+# build image
+cd "$DOCKER_PATH" || echo "no such directory $DOCKER_PATH"
+echo "\nStarting docker build..."
+docker build -t "$TARGET" .
+
+
+# start container
+echo "\nBuild finished. Running container in detached mode."
+CONTAINER_ID=$(docker run -d "$TARGET" /bin/sh) # cannot use --rm here: loses cid
+
+# copy binaries and stop container
+echo "\nCopying binaries..."
+docker cp -q "$CONTAINER_ID":/bin/geth-local "$OUTPUT_DIR"/geth-local
+docker cp -q "$CONTAINER_ID":/bin/geth-reference "$OUTPUT_DIR"/geth-reference
+docker stop "$CONTAINER_ID"
+docker rm "$CONTAINER_ID"
+
+
+# check binary and diff if neq
+md5_reference=$(md5 "$OUTPUT_DIR"/geth-reference | awk '{print $NF}')
+md5_local=$(md5 "$OUTPUT_DIR"/geth-local | awk '{print $NF}')
+
+echo "\nReference build has hash $md5_reference\nReproducing build has hash $md5_local"
+
+if [ "$md5_local" != "$md5_reference" ]; then
+    echo "\nBinaries mismatch. Running diffoscope..."
+    cd "$OUTPUT_DIR" || exit 1
+    docker run --rm -t -w "$(pwd)" -v "$(pwd)":"$(pwd)":ro registry.salsa.debian.org/reproducible-builds/diffoscope geth-reference geth-local
+else
+    echo "Binaries match."
+fi
+
+
+# todo write diff to file.
