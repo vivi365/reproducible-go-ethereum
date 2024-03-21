981f6b04bcfcb164ec209ca3834ae2364374ce21
anderssonvivi <viviandersson0@gmail.com>
2024-03-21 09:29:50 +0100

    script and docker refactoring

--- scripts/docker-and-diff.sh
+++ scripts/docker-and-diff.sh
@@ -14,7 +14,7 @@ DIR=$2
 DOCKER_PATH=~/reproducible-go-ethereum/docker/$DIR
 OUTPUT_DIR=~/reproducible-go-ethereum/bin
 DIFF_DIR=~/reproducible-go-ethereum/diff-explanations
-rm ~/reproducible-go-ethereum/bin/geth-local && rm ~/reproducible-go-ethereum/bin/geth-reference
+rm ~/reproducible-go-ethereum/bin/geth-1 && rm ~/reproducible-go-ethereum/bin/geth-2
 mkdir -p $OUTPUT_DIR $DIFF_DIR 
 
 
@@ -30,22 +30,22 @@ CONTAINER_ID=$(docker run -d "$TARGET" /bin/sh) # cannot use --rm here: loses ci
 
 # copy binaries and stop container
 echo "\nCopying binaries..."
-docker cp -q "$CONTAINER_ID":/bin/geth-local "$OUTPUT_DIR"/geth-local
-docker cp -q "$CONTAINER_ID":/bin/geth-reference "$OUTPUT_DIR"/geth-reference
+docker cp -q "$CONTAINER_ID":/bin/geth-1 "$OUTPUT_DIR"/geth-1
+docker cp -q "$CONTAINER_ID":/bin/geth-2 "$OUTPUT_DIR"/geth-2
 docker stop "$CONTAINER_ID"
 docker rm "$CONTAINER_ID"
 
 
 # check binary and diff if neq
-md5_reference=$(md5 "$OUTPUT_DIR"/geth-reference | awk '{print $NF}')
-md5_local=$(md5 "$OUTPUT_DIR"/geth-local | awk '{print $NF}')
+md5_reference=$(md5 "$OUTPUT_DIR"/geth-1 | awk '{print $NF}')
+md5_local=$(md5 "$OUTPUT_DIR"/geth-2 | awk '{print $NF}')
 
-echo "\nReference build has hash $md5_reference\nReproducing build has hash $md5_local"
+echo "\nFirst build has hash $md5_reference\nSecond build has hash $md5_local"
 
 if [ "$md5_local" != "$md5_reference" ]; then
     echo "\nBinaries mismatch. Running diffoscope..."
     cd "$OUTPUT_DIR" || exit 1
-    docker run --rm -t -w "$(pwd)" -v "$(pwd)":"$(pwd)":ro registry.salsa.debian.org/reproducible-builds/diffoscope geth-reference geth-local
+    docker run --rm -t -w "$(pwd)" -v "$(pwd)":"$(pwd)":ro registry.salsa.debian.org/reproducible-builds/diffoscope --progress --html $DIFF_DIR/diff.html geth-1 geth-2
 else
     echo "Binaries match."
 fi
